20d350cf2a17f5e79e23aa579cbc95b6ebad3a6d
Shay Zambrovski <shaike.zam@gmail.com>
2016-09-04 22:10:43 +0300

    [FEATURE] add about and register activity

--- app/src/main/java/com/example/shayzambrovski/digitalrecipe/MainScreen.java
+++ app/src/main/java/com/example/shayzambrovski/digitalrecipe/MainScreen.java
@@ -1,13 +1,53 @@
 package com.example.shayzambrovski.digitalrecipe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainScreen extends AppCompatActivity {
 
+    EditText userName;
+    EditText password;
+    Button logIn;
+    Button register;
+    Button about;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+        bindUI();
+    }
+    public void bindUI() {
+        this.userName = (EditText)findViewById(R.id.user_name_id);
+        this.password = (EditText)findViewById(R.id.password_id);
+        this.logIn = (Button)findViewById(R.id.log_in_id);
+        this.register = (Button)findViewById(R.id.register_id);
+        this.register.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                try{
+                    Intent myIntent = new Intent(MainScreen.this, RegisterScreen.class);
+                    startActivity(myIntent);
+                } catch(Exception e) {
+                    Log.e("Error: ", e.toString());
+                }
+            }
+        });
+        this.about = (Button)findViewById(R.id.about_id);
+        this.about.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                try{
+                    Intent myIntent = new Intent(MainScreen.this, AboutScreen.class);
+                    startActivity(myIntent);
+                } catch(Exception e) {
+                    Log.e("Error: ", e.toString());
+                }
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/shayzambrovski/digitalrecipe/RegisterScreen.java
@@ -0,0 +1,13 @@
+package com.example.shayzambrovski.digitalrecipe;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Shay Zambrovski on 04/09/2016.
+ */
+public class RegisterScreen extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_screen);
+    }
+}
